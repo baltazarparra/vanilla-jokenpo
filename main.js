6aec71815b889be3bc2703e6197b66a76cb3eb80
baltazarparra <baltazarparra@outlook.com>
2016-09-13 06:25:51 -0300

    primeiro refactor

--- main.js
+++ main.js
@@ -10,41 +10,53 @@
     var $userScore = document.querySelector('[data-js="userScore"]');
     var $cpuScore = document.querySelector('[data-js="cpuScore"]');
 
-    $rock.addEventListener('click', rockChoice, false);
-    $paper.addEventListener('click', paperChoice, false);
-    $scissor.addEventListener('click', scissorChoice, false);
+    var app = (function appController() {
+        return {
+            init: function init() {
+                this.initEvents();
+            },
 
-    function rockChoice() {
-        $userChoice.setAttribute("src", "rock.svg");
-        var cpuChoice = Math.random();
-        if (cpuChoice < 0.3) {
-            return $cpuChoice.setAttribute("src", "rock.svg"), $cpuScore.value = i;
-        } else if (cpuChoice < 0.6) {
-            return $cpuChoice.setAttribute("src", "paper.svg"), $cpuScore.value = i;
-        }
-        return $cpuChoice.setAttribute("src", "scissor.svg"), $userScore.value = i;
-    }
+            initEvents: function initEvents() {
+                $rock.addEventListener('click', this.rockChoice);
+                $paper.addEventListener('click', this.paperChoice);
+                $scissor.addEventListener('click', this.scissorChoice);
+            },
 
-    function paperChoice() {
-        $userChoice.setAttribute("src", "paper.svg");
-        var cpuChoice = Math.random();
-        if (cpuChoice < 0.3) {
-            return $cpuChoice.setAttribute("src", "rock.svg");
-        } else if (cpuChoice < 0.6) {
-            return $cpuChoice.setAttribute("src", "paper.svg");
-        }
-        return $cpuChoice.setAttribute("src", "scissor.svg");
-    }
+            rockChoice: function rockChoice() {
+                $userChoice.setAttribute("src", "rock.svg");
+                var cpuChoice = Math.random();
+                if (cpuChoice < 0.3) {
+                    return $cpuChoice.setAttribute("src", "rock.svg");
+                } else if (cpuChoice < 0.6) {
+                    return $cpuChoice.setAttribute("src", "paper.svg");
+                }
+                return $cpuChoice.setAttribute("src", "scissor.svg");
+            },
 
-    function scissorChoice() {
-        $userChoice.setAttribute("src", "scissor.svg");
-        var cpuChoice = Math.random();
-        if (cpuChoice < 0.3) {
-            return $cpuChoice.setAttribute("src", "rock.svg");
-        } else if (cpuChoice < 0.6) {
-            return $cpuChoice.setAttribute("src", "paper.svg");
-        }
-        return $cpuChoice.setAttribute("src", "scissor.svg");
-    }
+            paperChoice: function paperChoice() {
+                $userChoice.setAttribute("src", "paper.svg");
+                var cpuChoice = Math.random();
+                if (cpuChoice < 0.3) {
+                    return $cpuChoice.setAttribute("src", "rock.svg");
+                } else if (cpuChoice < 0.6) {
+                    return $cpuChoice.setAttribute("src", "paper.svg");
+                }
+                return $cpuChoice.setAttribute("src", "scissor.svg");
+            },
+
+            scissorChoice: function scissorChoice() {
+                $userChoice.setAttribute("src", "scissor.svg");
+                var cpuChoice = Math.random();
+                if (cpuChoice < 0.3) {
+                    return $cpuChoice.setAttribute("src", "rock.svg");
+                } else if (cpuChoice < 0.6) {
+                    return $cpuChoice.setAttribute("src", "paper.svg");
+                }
+                return $cpuChoice.setAttribute("src", "scissor.svg");
+            }
+        };
+    })();
+
+    app.init();
 
 })(window, document);
