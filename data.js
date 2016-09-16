f94d5a9a1339641191e7509b52f958b1f97b53c7
baltazarparra <baltazarparra@outlook.com>
2016-09-16 10:50:21 -0300

    first release

--- data.js
+++ data.js
@@ -4,6 +4,9 @@
 
     var $userScore = document.querySelector('[data-js="userScore"]');
     var $cpuScore = document.querySelector('[data-js="cpuScore"]');
+    var $rockOption = document.querySelector('[data-js="rockOption"]');
+    var $paperOption = document.querySelector('[data-js="paperOption"]');
+    var $scissorOption = document.querySelector('[data-js="scissorOption"]');
 
     var jokenpo = (function dataController(user, cpu) {
         var userScore = 0;
@@ -11,37 +14,42 @@
         return {
 
             play: function play(user, cpu) {
-
                 if (user === 'rock') {
-
-                    if (cpu === 'rock') {
-                        return jokenpo.handleScore('draw');
-                    } else if (cpu === 'paper') {
-                        return jokenpo.handleScore('lose');
-                    } else if (cpu === 'scissor'){
-                        return jokenpo.handleScore('win');
-                    }
-
+                    jokenpo.rockChoice(cpu);
                 } else if (user === 'paper') {
-
-                    if (cpu === 'paper') {
-                        return jokenpo.handleScore('draw');
-                    } else if (cpu === 'scissor') {
-                        return jokenpo.handleScore('lose');
-                    } else if (cpu === 'rock'){
-                        return jokenpo.handleScore('win');
-                    }
-
+                    jokenpo.paperChoice(cpu);
                 } else if (user === 'scissor') {
+                    jokenpo.scissorChoice(cpu);
+                }
+            },
 
-                    if (cpu === 'scissor') {
-                        return jokenpo.handleScore('draw');
-                    } else if (cpu === 'rock') {
-                        return jokenpo.handleScore('lose');
-                    } else if (cpu === 'paper'){
-                        return jokenpo.handleScore('win');
-                    }
+            rockChoice: function rockChoice(cpu) {
+                if (cpu === 'rock') {
+                    return jokenpo.handleScore('draw');
+                } else if (cpu === 'paper') {
+                    return jokenpo.handleScore('lose');
+                } else if (cpu === 'scissor'){
+                    return jokenpo.handleScore('win');
+                }
+            },
 
+            paperChoice: function paperChoice(cpu) {
+                if (cpu === 'paper') {
+                    return jokenpo.handleScore('draw');
+                } else if (cpu === 'scissor') {
+                    return jokenpo.handleScore('lose');
+                } else if (cpu === 'rock'){
+                    return jokenpo.handleScore('win');
+                }
+            },
+
+            scissorChoice: function scissorChoice(cpu) {
+                if (cpu === 'scissor') {
+                    return jokenpo.handleScore('draw');
+                } else if (cpu === 'rock') {
+                    return jokenpo.handleScore('lose');
+                } else if (cpu === 'paper'){
+                    return jokenpo.handleScore('win');
                 }
             },
 
@@ -55,19 +63,31 @@
 
             userPoint: function userPoint() {
                 userScore += 1;
-                $userScore.value = userScore
+                $userScore.value = userScore;
                 if (userScore > 2) {
                     $userScore.value = 'Win';
                     $cpuScore.value = 'Lose';
+                    $paperOption.setAttribute("src", "refresh.svg");
+                    $rockOption.setAttribute("src", "");
+                    $scissorOption.setAttribute("src", "");
+                    $paperOption.addEventListener('click', function(){
+                        document.location.reload(true);
+                    });
                 }
             },
 
             cpuPoint: function cpuPoint() {
                 cpuScore += 1;
-                $cpuScore.value = cpuScore
+                $cpuScore.value = cpuScore;
                 if (cpuScore > 2) {
                     $userScore.value = 'Lose';
                     $cpuScore.value = 'Win';
+                    $paperOption.setAttribute("src", "refresh.svg");
+                    $rockOption.setAttribute("src", "");
+                    $scissorOption.setAttribute("src", "");
+                    $paperOption.addEventListener('click', function(){
+                        document.location.reload(true);
+                    });  
                 }
             }
         };
